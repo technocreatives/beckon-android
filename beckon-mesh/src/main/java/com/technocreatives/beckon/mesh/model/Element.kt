5771ee9fd06d7701f51ef3d9c67f7eaa8048d0d0
Thanh Le <thanh@technocreatives.com>
2021-08-24 09:18:56 +0200

    Add index for Element

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/Element.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/Element.kt
@@ -4,7 +4,7 @@ import no.nordicsemi.android.mesh.models.VendorModel as NrfVendorModel
 import no.nordicsemi.android.mesh.transport.Element as NrfElement
 import no.nordicsemi.android.mesh.transport.MeshModel as NrfMeshModel
 
-class Element(private val element: NrfElement) {
+class Element(private val element: NrfElement, val index: Int) {
     val models get() = element.meshModels.map { it.value.to(it.key) }
     val address get() = element.elementAddress
 }
