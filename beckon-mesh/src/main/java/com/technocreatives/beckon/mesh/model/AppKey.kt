830bd07b19500553d5deae2d74c10bc44f6cfe02
David Göransson <david.goransson90@gmail.com>
2021-08-26 17:09:19 +0200

    Expose appkey

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/AppKey.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/AppKey.kt
@@ -3,7 +3,7 @@ package com.technocreatives.beckon.mesh.model
 import no.nordicsemi.android.mesh.ApplicationKey
 
 class AppKey(private val actualKey: ApplicationKey) {
-    internal val applicationKey get() = actualKey
+    val applicationKey get() = actualKey
     val keyIndex get() = actualKey.keyIndex
 
     val key = actualKey.key
