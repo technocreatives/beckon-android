b25f911b0a23ce5ce2d58bae9be03d8196fd4ce6
David Göransson <david.goransson90@gmail.com>
2021-08-27 13:45:41 +0200

    Expose nodename

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/UnprovisionedNode.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/UnprovisionedNode.kt
@@ -3,4 +3,10 @@ package com.technocreatives.beckon.mesh.model
 import no.nordicsemi.android.mesh.provisionerstates.UnprovisionedMeshNode
 
 class UnprovisionedNode(internal val node: UnprovisionedMeshNode) {
+    var name: String
+        set(value) {
+            node.nodeName = value
+        }
+        get() = node.nodeName
+
 }
