ee5ff8d3a5061e08514ad02fd203433e916256a8
Thanh Le <thanh@technocreatives.com>
2021-09-09 09:27:18 +0200

    Add uuid for UnprovisinedNode

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/UnprovisionedNode.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/UnprovisionedNode.kt
@@ -3,6 +3,7 @@ package com.technocreatives.beckon.mesh.model
 import no.nordicsemi.android.mesh.provisionerstates.UnprovisionedMeshNode
 
 class UnprovisionedNode(internal val node: UnprovisionedMeshNode) {
+    val uuid get() = node.deviceUuid
     var name: String
         set(value) {
             node.nodeName = value
