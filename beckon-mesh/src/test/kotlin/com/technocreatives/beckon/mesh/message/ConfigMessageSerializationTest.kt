b4c80a3c7678d54235368163a0c3c8175f7310f3
Thanh Le <thanh@technocreatives.com>
2021-11-01 14:16:21 +0100

    Fix tests

--- beckon-mesh/src/test/kotlin/com/technocreatives/beckon/mesh/message/ConfigMessageSerializationTest.kt
+++ beckon-mesh/src/test/kotlin/com/technocreatives/beckon/mesh/message/ConfigMessageSerializationTest.kt
@@ -28,8 +28,8 @@ class ConfigMessageSerializationTest : StringSpec({
     }
 
     "list of messages decode & encode" {
-        val netKey = MeshConfig.randomNetKey(0, 0)
-        val appKey = MeshConfig.randomAppKey(0, 0)
+        val netKey = MeshConfigHelper.randomNetKey(0, 0)
+        val appKey = MeshConfigHelper.randomAppKey(0, 0)
         val messages = listOf(
             GetCompositionData(1),
             GetDefaultTtl(1),
