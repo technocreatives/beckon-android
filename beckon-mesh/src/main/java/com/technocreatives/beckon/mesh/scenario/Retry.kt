e78a5919c919c4f511c0e978d512e707ded1c6f8
Thanh Le <thanh@technocreatives.com>
2022-02-02 14:19:45 +0100

    Add log for ExponentialBackoff

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/scenario/Retry.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/scenario/Retry.kt
@@ -29,8 +29,9 @@ data class RepeatRetry(val n: Int) : Retry {
     }
 }
 
-class ExponentialBackOffRetry(val maxRepeat: Int, val totalTime: Int) : Retry {
+data class ExponentialBackOffRetry(val maxRepeat: Int, val totalTime: Int) : Retry {
     override suspend fun <E, A> invoke(f: suspend () -> Either<E, A>): Either<E, A> {
+        Timber.w("ExponentialBackOffRetry 1")
         val sc = getSchedule<E, A>(maxRepeat, totalTime)
         return sc.repeat { f() }
     }
@@ -41,14 +42,18 @@ class ExponentialBackOffRetry(val maxRepeat: Int, val totalTime: Int) : Retry {
 @OptIn(ExperimentalTime::class)
 fun <A> exp(max: Int, totalTime: Int) = Schedule.exponential<A>(1.seconds).whileOutput {
     println("it ${it.inWholeSeconds}")
+    Timber.w("exp while ${it.inWholeSeconds}")
     it.inWholeSeconds < totalTime.seconds.inWholeSeconds
 } zipLeft max(max)
 
-fun <A> max(max: Int) = Schedule.recurs<A>(5)
+fun <A> max(max: Int) = Schedule.recurs<A>(max)
 
 
 @OptIn(ExperimentalTime::class)
 fun <A, B> getSchedule(max: Int, totalTime: Int): ESchedule<A, B> =
-    Schedule.doWhile<Either<A, B>> { it.isLeft() } zipLeft exp(max, totalTime)
+    Schedule.doWhile<Either<A, B>> {
+        Timber.w("Execute error $it")
+        it.isLeft()
+    } zipLeft exp(max, totalTime)
 
 typealias ESchedule<A, B> = Schedule<Either<A, B>, Either<A, B>>
