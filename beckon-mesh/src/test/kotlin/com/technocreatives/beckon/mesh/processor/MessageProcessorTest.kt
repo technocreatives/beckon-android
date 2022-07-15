a2a12b2434d1f195b10bb8df06c9f2454dca427c
David Göransson <david.goransson90@gmail.com>
2022-07-15 10:53:08 +0200

    Add test for shouldSendMessageImmediately

--- beckon-mesh/src/test/kotlin/com/technocreatives/beckon/mesh/processor/MessageProcessorTest.kt
+++ beckon-mesh/src/test/kotlin/com/technocreatives/beckon/mesh/processor/MessageProcessorTest.kt
@@ -1,11 +1,14 @@
 package com.technocreatives.beckon.mesh.processor
 
+import arrow.core.Either
 import arrow.core.right
+import com.technocreatives.beckon.mesh.SendMessageError
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import no.nordicsemi.android.mesh.transport.MeshMessage
@@ -18,6 +21,13 @@ class MessageProcessorTest : StringSpec({
     fun createMockMeshMessage(): MeshMessage {
         val meshMessage = mockk<MeshMessage>()
         every { meshMessage.message } returns null
+        return meshMessage
+    }
+
+    fun createMockMeshMessage(opCode: Int): MeshMessage {
+        val meshMessage = mockk<MeshMessage>()
+        every { meshMessage.message } returns null
+        every { meshMessage.opCode } returns opCode
         return meshMessage
     }
 
@@ -92,5 +102,30 @@ class MessageProcessorTest : StringSpec({
         result.isLeft() shouldBe true
         inputJob.cancel()
         processorJob.cancel()
+    }
+
+    "Should send immediately returns false while same responseOpCode is being processed" {
+        val queue = MessageQueue()
+        val destination = 0x0001
+        val responseOpCode = 0xFF
+        val otherResponseOpCode = 0xFE
+        val message = createMockMeshMessage(0x01)
+        val message2 = createMockMeshMessage(0x02)
+        val message3 = createMockMeshMessage(0x03)
+
+        val ackedMessage = BeckonMessage(1, Ack(1, destination, message, mockk(), responseOpCode), mockk())
+        val ackedMessage2 = BeckonMessage(2, Ack(2, destination, message2, mockk(), responseOpCode), mockk())
+        val ackedMessage3 = BeckonMessage(3, Ack(3, destination, message3, mockk(), otherResponseOpCode), mockk())
+
+        // Fake sending of message
+        queue.process(ackedMessage)
+        queue.clearProcessing()
+        // ackedMessage is now in mesh and we are waiting for response
+
+        // We should not send another message with same responseOpCode
+        queue.shouldSendMessageImmediately(ackedMessage2) shouldBe false
+
+        // Sending a message with other responseOpCode is fine
+        queue.shouldSendMessageImmediately(ackedMessage3) shouldBe true
     }
 })
