ee1d8bf7a9223b573ce2b5472510f5a137d8604e
Thanh Le <thanh@technocreatives.com>
2022-10-12 13:40:10 +0200

    More

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/scenario/TestCases.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/scenario/TestCases.kt
@@ -1,5 +1,22 @@
 package com.technocreatives.beckon.mesh.scenario
 
+import arrow.core.Either
+import arrow.core.continuations.either
+import com.technocreatives.beckon.mesh.BeckonMesh
+
 sealed interface Test {
-    data class AckMessage(val message: Message, val assert: (AckMessage) -> Boolean): Test
+    data class AckMessage(val message: Message, val assert: (AckMessage) -> Boolean) : Test
+}
+
+data class TestCase(val before: Scenario, val test: Test, val after: Scenario)
+
+class TestRunner(val beckonMesh: BeckonMesh) {
+    suspend fun run(case: TestCase) =
+        either {
+            with(case.before) { beckonMesh.execute() }.bind()
+            runTest(case.test).bind()
+            with(case.after) { beckonMesh.execute() }.bind()
+        }
+
+    private suspend fun runTest(test: Test): Either<Any, Unit> = TODO()
 }
