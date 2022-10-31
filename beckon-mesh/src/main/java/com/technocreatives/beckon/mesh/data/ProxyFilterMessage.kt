3af48c87a539239b8e3f628d744dcaad2d39fa03
Thanh Le <thanh@technocreatives.com>
2022-10-31 15:45:18 +0100

    more

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/data/ProxyFilterMessage.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/data/ProxyFilterMessage.kt
@@ -1,5 +1,3 @@
 package com.technocreatives.beckon.mesh.data
 
-import com.technocreatives.beckon.mesh.data.AccessPayload
-
 data class ProxyFilterMessage(val src: UnicastAddress, val payload: AccessPayload)
