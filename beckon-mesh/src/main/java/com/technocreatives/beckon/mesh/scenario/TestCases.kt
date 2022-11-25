c2365ea42f8e1d73682b382e08b8c123cb5d7a09
Thanh Le <thanh@technocreatives.com>
2022-11-25 10:53:52 +0100

    Support success rate/reliability

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/scenario/TestCases.kt
+++ beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/scenario/TestCases.kt
@@ -5,6 +5,7 @@ import arrow.core.continuations.either
 import arrow.fx.coroutines.parZip
 import arrow.typeclasses.Semigroup
 import com.technocreatives.beckon.mesh.BeckonMesh
+import com.technocreatives.beckon.mesh.data.GroupAddress
 import com.technocreatives.beckon.mesh.data.ProxyFilterMessage
 import com.technocreatives.beckon.mesh.data.PublishableAddress
 import com.technocreatives.beckon.mesh.data.UnicastAddress
@@ -18,6 +19,7 @@ import kotlinx.coroutines.withTimeoutOrNull
 import no.nordicsemi.android.mesh.transport.VendorModelMessageStatus
 import timber.log.Timber
 import kotlin.experimental.ExperimentalTypeInference
+import kotlin.math.roundToInt
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
@@ -30,10 +32,11 @@ sealed interface Test {
         val assert: Assertion<VendorModelMessageStatus>
     ) : Test
 
-    data class VendorMessage(
-        val address: PublishableAddress,
+    // for reliability / success-rate
+    data class SuccessRate(
+        val address: GroupAddress,
         val message: SendVendorModelMessage,
-        val expected: Set<ProxyFilterMessage>
+        val expected: ExpectedSuccessRate,
     ) : Test
 
     data class SingleVendorMessage(
@@ -46,10 +49,15 @@ sealed interface Test {
         val vendorMessages: List<Test>
     ) : Test
 
-    object Empty : Test
-
     data class Action(val execute: suspend () -> Either<TestFailed, Unit>) : Test
 
+    data class Repeat(val times: Int, val test: Test) : Test
+
+    object Empty : Test
+}
+
+data class ExpectedSuccessRate(val total: Int, val expectedPercentage: Float, val timeout: Duration = 30.seconds) {
+    fun expectedSize(): Int = (total * expectedPercentage).roundToInt()
 }
 
 interface Assertion<T> {
@@ -66,32 +74,30 @@ internal fun <T> Iterable<T>.collectionSizeOrDefault(default: Int): Int =
 @OverloadResolutionByLambdaReturnType
 public inline fun <E, A, B> Iterable<A>.mapAccumulating(
     f: (A) -> Either<E, B>
-): Either<Nel<E>, List<B>> =
-    Semigroup.nonEmptyList<E>().run {
-        fold(Either.Right(ArrayList<B>(collectionSizeOrDefault(10))) as Either<Nel<E>, MutableList<B>>) { acc, a ->
-            when (val res = f(a)) {
-                is Either.Right -> when (acc) {
-                    is Either.Right -> acc.also { it.value.add(res.value) }
-                    is Either.Left -> acc
-                }
-                is Either.Left -> when (acc) {
-                    is Either.Right -> nonEmptyListOf(res.value).left()
-                    is Either.Left -> acc.value.combine(nonEmptyListOf(res.value)).left()
-                }
+): Either<Nel<E>, List<B>> = Semigroup.nonEmptyList<E>().run {
+    fold(Either.Right(ArrayList<B>(collectionSizeOrDefault(10))) as Either<Nel<E>, MutableList<B>>) { acc, a ->
+        when (val res = f(a)) {
+            is Either.Right -> when (acc) {
+                is Either.Right -> acc.also { it.value.add(res.value) }
+                is Either.Left -> acc
+            }
+            is Either.Left -> when (acc) {
+                is Either.Right -> nonEmptyListOf(res.value).left()
+                is Either.Left -> acc.value.combine(nonEmptyListOf(res.value)).left()
             }
         }
     }
+}
 
 class TestRunner(val beckonMesh: BeckonMesh) {
-    suspend fun run(case: TestCase) =
-        either {
-            Timber.d("Setup")
-            with(case.before) { beckonMesh.execute() }.bind()
-            Timber.d("Running test")
-            case.tests.mapAccumulating { runTest(it) }.bind()
-            Timber.d("After")
-            with(case.after) { beckonMesh.execute() }.bind()
-        }
+    suspend fun run(case: TestCase) = either {
+        Timber.d("Setup")
+        with(case.before) { beckonMesh.execute() }.bind()
+        Timber.d("Running test")
+        case.tests.mapAccumulating { runTest(it) }.bind()
+        Timber.d("After")
+        with(case.after) { beckonMesh.execute() }.bind()
+    }
 
     private suspend fun runTest(test: Test): Either<TestFailed, Unit> = either {
         Timber.d("Running test $test")
@@ -99,11 +105,13 @@ class TestRunner(val beckonMesh: BeckonMesh) {
             beckonMesh.connectedState().mapLeft { TestFailed.NotInConnectedSTate }.bind()
         when (test) {
             is Test.VendorMessageAck -> connected.runTest(test).bind()
-            is Test.VendorMessage -> connected.runTest(test).bind()
+            is Test.SuccessRate -> connected.runTest(test).bind()
             is Test.SingleVendorMessage -> connected.runTest(test).bind()
             is Test.MultipleVendorMessage -> connected.runTest(test).bind()
             Test.Empty -> Unit.right()
             is Test.Action -> test.execute()
+            is Test.Repeat -> List(test.times) { runTest(test.test) }.mapAccumulating { it }
+                .bimap({ TestFailed.MultipleFailed(it) }) {}.bind()
         }
     }
 
@@ -113,17 +121,14 @@ class TestRunner(val beckonMesh: BeckonMesh) {
 
     private suspend fun Connected.runTest(test: Test.SingleVendorMessage): Either<TestFailed, Unit> =
         either {
-            val result: ProxyFilterMessage = parZip(
-                {
-                    beckonMesh.proxyFilterMessages().take(1)
-                        .collectUntil(30.seconds)
-                },
-                {
-                    sendVendorModelMessage(test.address, test.message)
-                        .mapLeft { TestFailed.ExecutionError(it) }
-                        .bind()
-                }
-            ) { r, _ -> r[0] }
+            val result: ProxyFilterMessage = parZip({
+                beckonMesh.proxyFilterMessages().take(1).collectUntil(30.seconds)
+            }, {
+                sendVendorModelMessage(
+                    test.address,
+                    test.message
+                ).mapLeft { TestFailed.ExecutionError(it) }.bind()
+            }) { r, _ -> r[0] }
 
             test.assert.assert(result).bind()
 
@@ -131,36 +136,32 @@ class TestRunner(val beckonMesh: BeckonMesh) {
 
     private suspend fun Connected.runTest(test: Test.VendorMessageAck): Either<TestFailed, Unit> =
         either {
-            val respond =
-                sendVendorModelMessageAck(test.address, test.message, test.responseOpCode)
-                    .mapLeft { TestFailed.ExecutionError(it) }
-                    .bind()
+            val respond = sendVendorModelMessageAck(
+                test.address,
+                test.message,
+                test.responseOpCode
+            ).mapLeft { TestFailed.ExecutionError(it) }.bind()
             test.assert.assert(respond).bind()
         }
 
-    private suspend fun Connected.runTest(test: Test.VendorMessage): Either<TestFailed, Unit> =
+    private suspend fun Connected.runTest(test: Test.SuccessRate): Either<TestFailed, Unit> =
         either {
 
-            val result: List<ProxyFilterMessage> = parZip(
-                {
-                    beckonMesh.proxyFilterMessages().take(test.expected.size)
-                        .collectUntil(30.seconds)
-                },
-                {
-                    sendVendorModelMessage(test.address, test.message)
-                        .mapLeft { TestFailed.ExecutionError(it) }
-                        .bind()
-                }
-            ) { r, _ -> r }
+            val result: List<ProxyFilterMessage> = parZip({
+                beckonMesh.proxyFilterMessages().take(test.expected.total).collectUntil(test.expected.timeout)
+            }, {
+                sendVendorModelMessage(
+                    test.address,
+                    test.message
+                ).mapLeft { TestFailed.ExecutionError(it) }.bind()
+            }) { r, _ -> r }
 
             Timber.d("ProxyFilterMessages $result")
 
-            ensure(result.toSet() == test.expected) {
+            ensure(result.size >= test.expected.expectedSize()) {
                 Timber.w("Failed $result")
                 TestFailed.NotEqual(
-                    test,
-                    test.expected,
-                    result.toSet()
+                    test, result
                 )
             }
         }
@@ -174,7 +175,7 @@ suspend fun <A> Flow<A>.collectUntil(duration: Duration): List<A> {
 
 sealed interface TestFailed {
     data class ExecutionError(val error: Any) : TestFailed
-    data class NotEqual<T>(val test: Test, val expected: T, val actual: T) : TestFailed
+    data class NotEqual<T>(val test: Test, val actual: T) : TestFailed
     data class MultipleFailed(val fails: Nel<TestFailed>) : TestFailed
     object NotInConnectedSTate : TestFailed
 }
