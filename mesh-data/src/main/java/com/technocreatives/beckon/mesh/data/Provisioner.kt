b51122012a23aa648bb76a5f375126781c9a6f9e
David Göransson <david.goransson90@gmail.com>
2022-12-07 10:14:39 +0100

    Remove non config value

--- mesh-data/src/main/java/com/technocreatives/beckon/mesh/data/Provisioner.kt
+++ mesh-data/src/main/java/com/technocreatives/beckon/mesh/data/Provisioner.kt
@@ -2,7 +2,6 @@ package com.technocreatives.beckon.mesh.data
 
 import arrow.optics.optics
 import com.technocreatives.beckon.mesh.data.serializer.HexToIntSerializer
-import com.technocreatives.beckon.mesh.data.serializer.UuidSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.util.*
@@ -16,8 +15,7 @@ data class Provisioner(
     val id: NodeId,
     val allocatedUnicastRange: List<AddressRange>, // TODO Should be special kind of range where 0x0001 to 0x7FFF is allowed
     val allocatedGroupRange: List<AddressRange>,// TODO Should be special kind of range where 0xC000 to 0xFFFF is allowed
-    val allocatedSceneRange: List<SceneRange>,
-    @Transient val isLastSelected: Boolean = false,
+    val allocatedSceneRange: List<SceneRange>
 ) {
     companion object
 }
