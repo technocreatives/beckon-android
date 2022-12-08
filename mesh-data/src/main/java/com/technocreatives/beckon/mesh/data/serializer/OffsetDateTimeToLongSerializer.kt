fee3e5a35f794b54199dff2157d78a623937536e
Thanh Le <thanh@technocreatives.com>
2022-12-08 14:51:25 +0100

    Fix timestamp serialization

--- mesh-data/src/main/java/com/technocreatives/beckon/mesh/data/serializer/OffsetDateTimeToLongSerializer.kt
+++ mesh-data/src/main/java/com/technocreatives/beckon/mesh/data/serializer/OffsetDateTimeToLongSerializer.kt
@@ -11,16 +11,18 @@ import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
 import java.time.OffsetDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @ExperimentalSerializationApi
 @Serializer(forClass = Long::class)
 object OffsetDateTimeToLongSerializer : KSerializer<Long> {
+    private val fmt: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
     override val descriptor: SerialDescriptor
         get() = PrimitiveSerialDescriptor("offsetDateTime", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Long) {
         val date = OffsetDateTime.ofInstant(Instant.ofEpochMilli(value), ZoneId.of("Z"))
-        encoder.encodeString(date.toString())
+        encoder.encodeString(fmt.format(date))
     }
 
     override fun deserialize(decoder: Decoder): Long {
