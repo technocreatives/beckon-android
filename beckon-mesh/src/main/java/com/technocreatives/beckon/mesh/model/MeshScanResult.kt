7cb95363b6c46d42c1c781557fba399749c7514d
Thanh Le <thanh@technocreatives.com>
2022-12-20 16:35:15 +0100

    wip

--- beckon-mesh/src/main/java/com/technocreatives/beckon/mesh/model/MeshScanResult.kt
+++
@@ -1,7 +0,0 @@
-package com.technocreatives.beckon.mesh.model
-
-import com.technocreatives.beckon.mesh.data.NetworkId
-import no.nordicsemi.android.support.v18.scanner.ScanRecord
-
-// todo scanRecord => mac address
-data class MeshScanResult(val scanRecord: ScanRecord, val networkId: NetworkId?)
